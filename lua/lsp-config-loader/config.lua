63d2dd78bb8aebbf74d79d6039e9f58e7e55d9f7
陆离 <sirzenith@163.com>
2023-12-30 01:56:12 +0800

    init

---
+++ lua/lsp-config-loader/config.lua
@@ -0,0 +1,15 @@
+local M = {
+    root_path = vim.fn.stdpath("config"),
+    log_update_method = "append",
+    log_scroll_method = "bottom",
+    on_attach_callbacks = {},
+    capabilities_settings = {},
+    format_args = {
+        async = true
+    },
+    kind_label = {},
+    server_config = {},
+    server_list = {},
+}
+
+return M
---
+++ lua/lsp-config-loader/loader.lua
@@ -0,0 +1,121 @@
+local lsp_status_ok, lsp_status = pcall(require, "lsp-status")
+if not lsp_status_ok then
+    lsp_status = nil
+end
+
+local fs = vim.fs
+
+local module_config = require "user-lsp.config"
+
+local M = {}
+
+---@param client lsp.Client
+---@param bufnr number
+local function lsp_on_attach(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+    -- See `:help vim.lsp.*` for documentation on any one of following functions
+    local keymap = {
+        -- utility
+        ["<A-F>"] = function() vim.lsp.buf.format(module_config.format_args) end,
+        ["<F2>"] = vim.lsp.buf.rename,
+        ["<space>ca"] = vim.lsp.buf.code_action,
+        -- goto
+        ["gd"] = vim.lsp.buf.definition,
+        ["gD"] = vim.lsp.buf.declaration,
+        ["gr"] = vim.lsp.buf.references,
+        ["gi"] = vim.lsp.buf.implementation,
+        -- diagnostic
+        ["<A-n>"] = vim.diagnostic.goto_prev,
+        ["<A-.>"] = vim.diagnostic.goto_next,
+        ["<space>d"] = vim.diagnostic.setloclist,
+        ["<space>e"] = vim.diagnostic.open_float,
+        -- hover & detail
+        ["K"] = vim.lsp.buf.hover,
+        ["<C-k>"] = vim.lsp.buf.signature_help,
+        ["<space>D"] = vim.lsp.buf.type_definition,
+        -- workspace
+        ["<space>wa"] = vim.lsp.buf.add_workspace_folder,
+        ["<space>wr"] = vim.lsp.buf.remove_workspace_folder,
+        ["<space>wl"] = function()
+            local msg = table.concat(vim.lsp.buf.list_workspace_folders(), ",\n")
+            vim.notify(msg)
+        end,
+    }
+
+    local set = vim.keymap.set
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    for key, callback in pairs(keymap) do
+        set("n", key, callback, opts)
+    end
+
+    for _, callback in module_config.on_attach_callbacks:ipairs() do
+        callback(client, bufnr)
+    end
+end
+
+-- Try to find config file for given language server in user config directory.
+---@param ls_name string
+local function load_config_from_module(ls_name)
+    local user_config_path = fs.normalize(module_config.root_path) .. "/" .. ls_name .. ".lua"
+
+    local user_config
+    if vim.fn.filereadable(user_config_path) == 0 then
+        user_config = {}
+    else
+        local ok, module = xpcall(require, debug.traceback, user_config_path)
+        user_config = ok and module or {}
+    end
+
+    return user_config
+end
+
+-- Load config table for given language server. Resolve priority from low to high
+-- will be: some basic default, config in user cnfig directory, workspace user
+-- config.
+---@param ls_name string
+---@param user_config? table
+---@return table
+function M.load(ls_name, user_config)
+    local config = {
+        flags = {
+            debounce_text_changes = 150,
+        },
+    }
+
+    -- set capabilities
+    local capabilities = {}
+    for _, cap in ipairs(module_config.capabilities_settings) do
+        vim.tbl_extend("force", capabilities, cap)
+    end
+    config.capabilities = capabilities
+
+    -- lsp status plugin hook
+    local ext = lsp_status and lsp_status.extensions[ls_name]
+    if ext then
+        config.handlers = ext.setup()
+    end
+
+    -- merging
+    vim.tbl_extend(
+        "force",
+        config,
+        load_config_from_module(ls_name),
+        user_config or {}
+    )
+
+    -- wrapping on_attach
+    local on_attach = config.on_attach
+    config.on_attach = function(client, bufnr)
+        lsp_on_attach(client, bufnr)
+
+        if type(on_attach) == 'function' then
+            on_attach(client, bufnr)
+        end
+    end
+
+    return config
+end
+
+return M
